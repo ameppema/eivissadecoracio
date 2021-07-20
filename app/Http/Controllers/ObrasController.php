eb0fba00b77dee4ac591dd412994c9a775cb56e8
Paul Marquez <ameppema@hotmail.com>
2021-07-20 22:40:52 +0200

    Get galleries in Obras Controller

--- app/Http/Controllers/ObrasController.php
+++ app/Http/Controllers/ObrasController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Content;
+use App\Models\Galleries;
 
 class ObrasController extends Controller
 {
     public function index(){
         $content = Content::getContent('obras');
-        $gallery = Content::getGallery();
+        $galleryOne = Galleries::page(1)->gallery(1)->get();
+        $galleryTwo = Galleries::page(1)->gallery(2)->get();
         $menus = Content::getMenu();
 
-        return view('page', compact(['content', 'menus', 'gallery']));
+        return view('page', compact(['content', 'menus', 'galleryOne', 'galleryTwo']));
     }
 }
