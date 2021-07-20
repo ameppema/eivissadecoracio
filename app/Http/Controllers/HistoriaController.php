c092d293ab61e8f76bc5e5bfa51c96c1f95c6636
Paul Marquez <ameppema@hotmail.com>
2021-07-21 01:03:36 +0200

    Get galleries in Historia Controller

--- app/Http/Controllers/HistoriaController.php
+++ app/Http/Controllers/HistoriaController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Content;
+use App\Models\Galleries;
 
 class HistoriaController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         $content = Content::getContent('historia');
-        $gallery = Content::getGallery();
+        $galleryOne = Galleries::page(6)->gallery(1)->get();
+        $galleryTwo = Galleries::page(6)->gallery(2)->get();
         $menus = Content::getMenu();
 
-        return view('page',compact(['content', 'gallery', 'menus']));
+        return view('page', compact(['content', 'menus', 'galleryOne', 'galleryTwo']));
     }
 }
