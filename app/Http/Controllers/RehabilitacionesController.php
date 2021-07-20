1e1d91dc985bb9aaff7a37d0e3f5b29443dc28fa
Paul Marquez <ameppema@hotmail.com>
2021-07-21 01:04:38 +0200

    Get galleries in Rehabilitaciones Controller

--- app/Http/Controllers/RehabilitacionesController.php
+++ app/Http/Controllers/RehabilitacionesController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Content;
+use App\Models\Galleries;
 
 class RehabilitacionesController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         $content = Content::getContent('rehabilitaciones');
-        $gallery = Content::getGallery();
+        $galleryOne = Galleries::page(2)->gallery(1)->get();
+        $galleryTwo = Galleries::page(2)->gallery(2)->get();
         $menus = Content::getMenu();
 
-        return view('page', compact(['content', 'menus', 'gallery']));
+        return view('page', compact(['content', 'menus', 'galleryOne', 'galleryTwo']));
     }
 }
