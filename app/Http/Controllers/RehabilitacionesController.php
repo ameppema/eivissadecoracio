1d728c246b97397ffafd625b49e65ba59ebdf71f
Jean Langarica <jeanmacario048@gmail.com>
2021-07-22 15:44:11 -0500

    Errores de controladores solucionados

--- app/Http/Controllers/RehabilitacionesController.php
+++ app/Http/Controllers/RehabilitacionesController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Content;
+use App\Models\Galleries;
 
 class RehabilitacionesController extends Controller
 {
     public function index()
     {
         $content = Content::getContent('rehabilitaciones');
-        $gallery = Content::getGallery();
+        $galleryOne = Galleries::page(1)->gallery(1)->get();
+        $galleryTwo = Galleries::page(1)->gallery(2)->get();
         $menus = Content::getMenu();
 
-        return view('page', compact(['content', 'menus', 'gallery']));
+        return view('page', compact(['content', 'menus', 'galleryOne', 'galleryTwo']));
     }
 }
