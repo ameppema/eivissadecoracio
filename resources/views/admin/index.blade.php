c14c133d85aba9daf9b65aca466a05a16355a343
Paul Marquez <ameppema@hotmail.com>
2021-08-03 12:38:36 +0200

    Add links, items, styles & routes in cards

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,73 +10,71 @@
 
 @section('content')
     <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <p>
-                            Aquí puedes tener acceso para modificar todas las secciones de la web
-                        </p>
+        <div class="card">
+            <p class="card__intro">
+                Aquí puedes tener acceso para modificar todas las secciones de la web
+            </p>
 
-                        <div class="card-body">
-                            <a href="#">
-                                <div class="card_item">
-                                    <i class="fas fa-list-ul"></i>
-                                    <p>Menu</p>
-                                    <button>Click</button>
-                                </div>
-                            </a>
-                            
-                            <div class="card_item">
-                                <i class="far fa-images"></i>
-                                <p>Slider</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-landmark"></i>
-                                <p>Historia</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-hard-hat"></i>
-                                <p>Obras</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-toolbox"></i>
-                                <p>Rehabilitaciones</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-house-user"></i>
-                                <p>Interiores</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-utensils"></i>
-                                <p>Cocinas</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-briefcase"></i>
-                                <p>Parquets</p>
-                                <button>Click</button>
-                            </div>
-                            
-                            <div class="card_item">
-                                <i class="fas fa-handshake"></i>
-                                <p>Partners</p>
-                                <button>Click</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="container__card">
+                <a class="card__items" href="admin/category_menu">
+                    <i class="card__logo fas fa-list-ul"></i>
+                    <p class="card__text">Menu</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/slide">
+                    <i class="card__logo far fa-images"></i>
+                    <p class="card__text">Slider</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/historia/6">
+                    <i class="card__logo fas fa-landmark"></i>
+                    <p class="card__text">Historia</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/obras/1">
+                    <i class="card__logo fas fa-hard-hat"></i>
+                    <p class="card__text">Obras</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/rehabilitaciones/2">
+                    <i class="card__logo fas fa-tools"></i>
+                    <p class="card__text">Rehabilitaciones</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/interiores/3">
+                    <i class="card__logo fas fa-couch"></i>
+                    <p class="card__text">Interiores</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/cocinas/4">
+                    <i class="card__logo fas fa-utensils"></i>
+                    <p class="card__text">Cocinas</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/parquets/5">
+                    <i class="card__logo fas fa-briefcase"></i>
+                    <p class="card__text">Parquets</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="admin/module/partners">
+                    <i class="card__logo fas fa-handshake"></i>
+                    <p class="card__text">Partners</p>
+                    <button class="card__button">Click</button>
+                </a>
+                
+                <a class="card__items" href="#">
+                    <i class="card__logo fas fa-user"></i>
+                    <p class="card__text">Users</p>
+                    <button class="card__button">Click</button>
+                </a>
             </div>
         </div>
     </section>
@@ -89,7 +87,7 @@
             align-items: center;
         }
         
-        .card p {
+        .card__intro {
             width: 500px;
             margin: 40px 0 10px;
             text-align: center;
@@ -97,52 +95,60 @@
             line-height: 30px;
         }
 
-        .card-body {
+        .container__card {
             display: flex;
             flex-wrap: wrap;
             justify-content: center;
         }
         
-        .card_item {
+        .card__items {
             display: flex;
             flex-direction: column;
-            justify-content: center;
+            justify-content: space-around;
             align-items: center;
             width: 200px;
             height: 200px;
             margin: 20px;
-            border: 2px solid lightgrey;
+            padding: 15px 0;
             border-radius: 20px;
+            border: 2px solid lightgrey;
         }
         
-        .card_item:hover {
-            color: blue;
-            border: 2px solid blue;
-        }
-        
-        .card_item i {
+        .card__logo {
+            color: #212529;
             font-size: 40px;
-            margin-bottom: 10px;
         }
         
-        .card_item p {
-            margin: 10px 0;
+        .card__text {
+            width: 90%;
+            margin: 0;
             color: #212529;
             font-size: 18px;
+            text-align: center;
         }
         
-        .card_item button {
+        .card__button {
             width: 120px;
             height: 40px;
             border-radius: 10px;
             border: none;
             color: white;
-            background-color: #007bff;
+            background-color: #212529;
         }
-        
-        .card_item button:hover {
+
+        .card__items:hover {
+            color: #007bff;
+            border: 2px solid #007bff;
+            box-shadow: 6px 5px 5px #007bff1a;
+        }
+
+        .card__items:hover .card__logo  {
+            color: #007bff;
+        }
+
+        .card__items:hover .card__button  {
             color: white;
-            background-color: blue;
+            background-color: #007bff;
         }
     </style>
 @stop
