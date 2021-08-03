304b5a8a1a7729c743b36b3d99a7a836fb9a0bb3
Paul Marquez <ameppema@hotmail.com>
2021-08-03 18:01:59 +0200

    Removing unnecessary classes

--- resources/views/admin/modules/slider.blade.php
+++ resources/views/admin/modules/slider.blade.php
@@ -72,24 +72,6 @@
         .description__label {
             margin: 0 10px 0 5px;
         }
-        
-        .section__test {
-            margin-bottom: 0;
-            display: block;
-            width: 100%;
-            height: calc(2.25rem + 2px);
-            padding: .375rem .75rem;
-            font-size: 1rem;
-            font-weight: 400;
-            line-height: 1.5;
-            color: #495057;
-            background-color: #fff;
-            background-clip: padding-box;
-            border: 1px solid #ced4da;
-            border-radius: .25rem;
-            box-shadow: inset 0 0 0 transparent;
-            transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-        }
 
         .slider__title > .form-group,
         .slider__description > .form-group,
