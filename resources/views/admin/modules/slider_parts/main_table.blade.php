762b8e493720200f1cd91dc2a43d2639ca9b4f8a
Paul Marquez <ameppema@hotmail.com>
2021-08-09 11:58:24 +0200

    Fix columns width

--- resources/views/admin/modules/slider_parts/main_table.blade.php
+++ resources/views/admin/modules/slider_parts/main_table.blade.php
@@ -1,61 +1,71 @@
 <table class="table">
     <thead>
         <tr>
-            <th style="border-bottom: none;">#</th>
-
-            <th style="border-bottom: none;">Imagen</th>
-
-            <th style="border-bottom: none;">
+            {{-- Slide Order --}}
+            <th style="width: 5%; border-bottom: none;">#</th>
+            
+            {{-- Slide Image --}}
+            <th style="width: 15%; border-bottom: none;">Imagen</th>
+            
+            {{-- Slide Title Spanish --}}
+            <th style="width: 15%; border-bottom: none;">
                 <img class="title__image" src="/images/navbar/lang_es.png" alt="Eivissa Decoracio Spanish">
                 Título
             </th>
 
-            <th style="border-bottom: none;">
+            {{-- Slide Description Spanish --}}
+            <th style="width: 20%; border-bottom: none;">
                 <img class="title__image" src="/images/navbar/lang_es.png" alt="Eivissa Decoracio Spanish">
                 Descripción
             </th>
 
-            <th style="border-bottom: none;">
+            {{-- Slide Title English --}}
+            <th style="width: 15%; border-bottom: none;">
                 <img class="title__image" src="/images/navbar/lang_en.png" alt="Eivissa Decoracio English">
                 Título
             </th>
 
-            <th style="border-bottom: none;">
+            {{-- Slide Description English --}}
+            <th style="width: 20%; border-bottom: none;">
                 <img class="title__image" src="/images/navbar/lang_en.png" alt="Eivissa Decoracio English">
                 Descripción
             </th>
 
-            <th style="border-bottom: none;">Acciones</th>
+            {{-- Slide Edit --}}
+            <th style="width: 10%; border-bottom: none;">Acciones</th>
         </tr>
     </thead>
     
     <tbody id="table-edit">
         @foreach($slide as $slideItem)
         <tr>
-            {{-- Order Slide --}}
+            {{-- Slide Order --}}
             <th style="vertical-align: middle;" scope="row">{{$loop->iteration}}</th>
             
-            {{-- Image Slide --}}
+            {{-- Slide Image --}}
             <td style="width: 150px;"><img style="width: inherit;" src="/storage/{{$slideItem->imagen}}" class="card-img" alt="Slide Item"></td>
             
-            {{-- Title & Description in Spanish --}}
+            {{-- Slide Title Spanish --}}
             <td style="vertical-align: middle;">{{ $slideItem->titulo }}</td>
+            
+            {{-- Slide Description Spanish --}}
             <td style="vertical-align: middle;">{{$slideItem->descripcion}}</td>
             
-            {{-- Title & Description in English --}}
+            {{-- Slide Title English --}}
             <td style="vertical-align: middle;">{{ $slide_en[$loop->iteration - 1]->titulo }}</td>
+
+            {{-- Slide Description English --}}
             <td style="vertical-align: middle;">{{$slide_en[$loop->iteration - 1]->titulo}}</td>
 
-            {{-- Edit Slide --}}
+            {{-- Slide Actions --}}
             <td style="vertical-align: middle; padding: 0 .75rem;">
-                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalCustom" data-slideid="{{$slideItem->id}}">
+                {{-- Slide Edit --}}
+                <button class="btn btn-warning" type="button" data-toggle="modal" data-target="#modalCustom" data-slideid="{{$slideItem->id}}">
                     <i class="fas fa-pencil-alt" data-slideid="{{$slideItem->id}}"></i>
                 </button>
-            </td>
-            
-            {{-- Remove Slide --}}
-            <td style="vertical-align: middle; padding: 0;">
-                <form action="{{url('admin/slide/' .$slideItem->id)}}" method="POST">
+
+                {{-- Slide Remove --}}
+                <form style="display: inline;" action="{{url('admin/slide/' .$slideItem->id)}}" method="POST">
                     @method('delete')
                     @csrf
                     <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
