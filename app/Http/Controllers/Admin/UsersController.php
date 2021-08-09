260291513fcd2fa8ebab683a6b091795c023529f
Paul Marquez <ameppema@hotmail.com>
2021-08-09 20:38:02 +0200

    Create UsersController

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -14,6 +15,8 @@ class UsersController extends Controller
      */
     public function index()
     {
+        $users = User::all();
+
         return view('admin.modules.users');
     }
 
